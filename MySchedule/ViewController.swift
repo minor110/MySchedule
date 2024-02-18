be8870dff14ebe18887f1b24777fd7cf4ce531ad
minor110 <filippov.sergey110@gmail.com>
2024-02-18 22:10:42 +0300

    first commit

---
+++ MySchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySchedule
+//
+//  Created by Sergey Filippov on 18.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
